336105250e6f03ed38c0706a9de0767938a39620
Danielmc09 <admendieta9@misena.edu.co>
2021-06-22 23:42:44 -0500

    Adelanto UserVies

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.users.api.api import UserAPIVIew
+from apps.users.api.api import user_api_view, user_detail_view
 
 urlpatterns = [
-    path('usuarios/', UserAPIVIew.as_view(), name='usuarios'),
+    path('usuarios/', user_api_view, name='usuarios'),
+    path('usuarios/<int:pk>/', user_detail_view, name='usuarios_detail_api_view'),
 ]
